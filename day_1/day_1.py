3e1333587cb3d3bd8311e0b030f9ad1277f710ad
Ryan Mitwally <ryan.mitwally@edu.fh-joanneum.at>
2023-12-02 18:01:59 +0100

    Solved Day 1 Puzzle 2

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,28 +1,45 @@
 import os
+from re import search
 
 
 def find_digit(line: str, lr: bool):
+    pattern = r"(one|two|three|four|five|six|seven|eight|nine)"
+    mapping = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5",
+               "six": "6", "seven": "7", "eight": "8", "nine": "9"}
+
     if lr:
         for i in range(0, len(line)):
             if line[i].isdecimal():
                 return line[i]
+            else:
+                result = search(pattern, line[:i+1])
+                if result:
+                    return mapping[result.group(0)]
+                
     else:
         for i in range(len(line)-1, -1, -1):
             if line[i].isdecimal():
                 return line[i]
-    print(line)
+            else:
+                result = search(pattern, line[i:])
+                if result:
+                    return mapping[result.group(0)]
+
     raise Exception("No number in this line")
+
 
 with open(os.path.join(os.path.dirname(__file__), "input.txt")) as file:
     config_lines = file.readlines()
 
 total = 0
 
-for line in config_lines:
-    first = find_digit(line, True)
-    last = find_digit(line, False)
+for cfgline in config_lines:
+    first = find_digit(cfgline, True)
+    last = find_digit(cfgline, False)
     combined = int(first + last)
-    total+=combined
+    total += combined
+    #print(first, last, cfgline, combined)
+
 
 print(total)
 
