8438fe2814e934eda42a91ff5675a39f50263a39
Ryan Mitwally <ryan.mitwally@edu.fh-joanneum.at>
2023-12-02 21:09:53 +0100

    Solved puzzle 2 of day 2

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -12,22 +12,41 @@ def check_batches(valid, batches) -> bool:
     return True
 
 
+def get_line_pwr(batches: list) -> int:
+    power = 1
+    max_vals = {"red": 0, "blue": 0, "green": 0}
+    for batch in batches:
+        color = batch.strip().split(" ")[1]
+        amount = int(batch.strip().split(" ")[0])
+        if amount > max_vals[color]:
+            max_vals[color] = amount
+    for val in list(max_vals.values()):
+        power *= val
+    return power
+
+
+
 if __name__ == "__main__":
-    with open(os.path.join(os.path.dirname(__file__), "input.txt")) as file:
+    with open(os.path.join(os.path.dirname(__file__), "input.txt"), encoding="UTF-8") as file:
         config_lines = file.readlines()
 
     possible = {"red": 12, "green": 13, "blue": 14}
 
-    total = 0
+    total_valid = 0
+    power_sum = 0
 
     for line in config_lines:
         game = line.split(":")
         game_id = int(game[0].split(" ")[1])
         game_batches = game[1].split(";")
+
         game_batches[-1] = game_batches[-1].strip("\n")
         if check_batches(possible, game_batches):
-            total += game_id
+            total_valid += game_id
 
-    print(total)
+        power_batches = game[1].strip("\n").strip().replace(";", ",").split(",")
+        line_pwr = get_line_pwr(power_batches)
+        power_sum += line_pwr
 
-
+    print("valid: ", total_valid)
+    print("power sum: ", power_sum)
